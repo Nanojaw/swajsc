ba4eaa8b6b2a8a0b806b64561481ec58127197df
TuffenDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2023-05-08 17:41:32 +0200

    Added clap and test file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,31 @@ mod ast;
 mod codegen;
 mod patterns;
 
+use std::path::PathBuf;
+use std::fs;
+use std::io::{stdin, Read};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version)]
+/// Supercharging WebAssembly to JavaScript Compiler
+struct Cli {
+    #[arg(short, long, value_name = "FILE", help = "Path to the wasm module. Will read from stdin if ommited.")]
+    path: Option<PathBuf>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let src = match cli.path.as_deref() {
+        Some(path) => fs::read(path).expect("Could not read file from path!"),
+        None => stdin().bytes().collect::<Result<Vec<u8>, std::io::Error>>().expect("Could not read file from stdin!"),
+    };
+
+    let ast = match parser::parse_wasm(src.as_slice()) {
+        Ok(ast) => ast,
+        Err(error) => panic!("Module is invalid! Error: {}", error.message()),
+    };
+
+    codegen::generate_js(ast);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,9 @@
 use crate::ast;
 use wasmparser::validate;
 
-pub fn parse_wasm(src: &[u8]) -> Result<ast::Module, String> {
+pub fn parse_wasm(src: &[u8]) -> Result<ast::Module, wasmparser::BinaryReaderError> {
     // make sure that the module is valid before parsing
-    match validate(src) {
-        Err(e) => return Err(format!("Wasm module was invalid! Error message: {}", e.message())),
-        _ => ()
-    }
+    validate(src)?;
 
     todo!()
 }
